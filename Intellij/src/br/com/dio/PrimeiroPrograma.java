eb16c1e8942a8b4adc159b9c825b0aed38a6d0fa
Rebeca Palmeira <rebeca.palmeira@gmail.com>
2022-01-09 17:37:41 -0300

    first commit

---
+++ Intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato("Fuxico", "laranja", 5);
+        System.out.println(gato);
+    }
+}
